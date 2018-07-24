a66d34e5ac6053806cbb92572a437a0eb301de3c
manzochickdev <32512675+manzochickdev@users.noreply.github.com>
2018-07-24 22:25:40 +0700

    Cong Dong Truyen
    App đọc truyện

---
+++ AppX/FragmentGridView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using AppX.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AppX
+{
+    public sealed partial class FragmentGridView : UserControl
+    {
+        public FragmentGridView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void lvHomePage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width < 800)
+            {
+                var panel = (ItemsWrapGrid)lvHomePage.ItemsPanelRoot;
+                panel.ItemWidth = e.NewSize.Width / 2;
+            }
+            else
+            {
+                var panel = (ItemsWrapGrid)lvHomePage.ItemsPanelRoot;
+                panel.ItemWidth = e.NewSize.Width / 4;
+            }
+        }
+
+        private void lvHomePage_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Novel pass = (Novel)e.ClickedItem;
+            ((Frame)Window.Current.Content).Navigate(typeof(DetailPage), pass);
+        }
+
+    }
+}
---
+++ AppX/FragmentTop.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AppX
+{
+    public sealed partial class FragmentTop : UserControl
+    {
+
+        public FragmentTop()
+        {
+            this.InitializeComponent();            
+        }
+
+
+        private void iLogo_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(MainPage));
+        }
+
+
+    }
+
+
+}
